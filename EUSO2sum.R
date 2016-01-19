7b3a80e7f7350539db7b2cb20b431a949e341dfa
maierucsb <mmaier@umail.ucsb.edu>
2016-01-19 13:54:23 -0800

    New Change (Purposeful Error)

--- EUSO2sum.R
+++ EUSO2sum.R
@@ -16,21 +16,15 @@
 EUSO2 <- read.csv(file="EUSO2.csv")
 
 # Summarize countries by their mean SO2 concentration across all monitoring stations
-EUSO2CountryM = aggregate(ug.m3 ~ country.iso.code, data = EUSO2, FUN = mean)
+EUSO2CountryAgg = aggregate(ug.m3 ~ country.iso.code, data = EUSO2, FUN = mean)
 
 # Boxplot summary of SO2 measurements taken in France, Germany and Czech Republic
 
 #Creates Subset of Data
-<<<<<<< HEAD
-TriComp = subset(EUSO2, country.iso.code == "FR" | country.iso.code == "DE" | country.iso.code == "CZ")
-=======
-TriComp = subset(EUSO2, country.iso.code == "AT" | country.iso.code == "HU" | country.iso.code == "ES")
->>>>>>> toxins
+TriComp = subset(EUSO2CountryAgg, country.iso.code == "AT" | country.iso.code == "HU" | country.iso.code == "ES")
+
 #Removes now unused Data
 TriComp$country.iso.code <- droplevels(TriComp$country.iso.code)
 #Plots Data
 boxplot(ug.m3 ~ country.iso.code , data=TriComp, xlab = "Countries", ylab = "Atmosphertic SO2 (ug/m3)", main="SO2 Concentrations in Selected EU Countries")
-<<<<<<< HEAD
-=======
 
->>>>>>> toxins
