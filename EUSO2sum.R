bbe1eb98c96284242433658e2ad04a5b4014532f
sparrow925 <elise.k.wall@gmail.com>
2016-01-18 22:16:59 -0800

    Added aggregate for average SO2 levels by country AND a boxplot for France, Germany and the Czech Repub. The boxplot isn't finished.

--- EUSO2sum.R
+++ EUSO2sum.R
@@ -10,3 +10,12 @@
 
 # Not sure how to load files here... hmmm is this directory correct...
 EUSO2 <- read.csv("~EUSO2.csv")
+
+# Summarize countries by their mean SO2 concentration across all monitoring stations
+EUSO2CountryM = aggregate(ug.m3 ~ country.iso.code, data = EUSO2, FUN = mean)
+
+# Boxplot summary of SO2 measurements taken in France, Germany and Czech Republic
+# ... this boxplot has an issue with the country codes ALL showing up. Will fix later.
+TriComp = subset(EUSO2, country.iso.code == "FR" | country.iso.code == "DE" | country.iso.code == "CZ")
+TriComp = data.frame(TriComp)
+boxplot(ug.m3 ~ country.iso.code , data=TriComp, xlab = "Countries", ylab = "Atmosphertic SO2 (ug/m3)", main="SO2 Concentrations in Selected EU Countries")
