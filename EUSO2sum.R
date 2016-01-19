3a98de4a755332c392229358d34785e9546d93ef
maierucsb <mmaier@umail.ucsb.edu>
2016-01-19 13:25:39 -0800

    Fixed Boxplot and Uploading Errors

--- EUSO2sum.R
+++ EUSO2sum.R
@@ -8,14 +8,21 @@
 # Informatics Winter quarter 2016
 ####
 
-# Not sure how to load files here... hmmm is this directory correct...
-EUSO2 <- read.csv("~EUSO2.csv")
+
+#
+
+
+# Upload file EUSO2
+EUSO2 <- read.csv(file="EUSO2.csv")
 
 # Summarize countries by their mean SO2 concentration across all monitoring stations
 EUSO2CountryM = aggregate(ug.m3 ~ country.iso.code, data = EUSO2, FUN = mean)
 
 # Boxplot summary of SO2 measurements taken in France, Germany and Czech Republic
-# ... this boxplot has an issue with the country codes ALL showing up. Will fix later.
+
+#Creates Subset of Data
 TriComp = subset(EUSO2, country.iso.code == "FR" | country.iso.code == "DE" | country.iso.code == "CZ")
-TriComp = data.frame(TriComp)
+#Removes now unused Data
+TriComp$country.iso.code <- droplevels(TriComp$country.iso.code)
+#Plots Data
 boxplot(ug.m3 ~ country.iso.code , data=TriComp, xlab = "Countries", ylab = "Atmosphertic SO2 (ug/m3)", main="SO2 Concentrations in Selected EU Countries")
