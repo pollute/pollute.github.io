790d7903bc03e90d618398423f4c2ebe94ea8704
sparrow925 <elise.k.wall@gmail.com>
2016-01-18 21:37:21 -0800

    Added origional dataset and SO2 .csv. Created EUSO2sum for summary statistics and...more?

---
+++ EUSO2sum.R
@@ -0,0 +1,12 @@
+#### 
+# Air Quality Summary Statistics
+# Dataset: Air pollutant concentrations 2013 - Dataset by cities (compared to EU values)
+# Using SO2 tab
+# data taken from http://www.eea.europa.eu/data-and-maps/data/air-pollutant-concentrations-at-station
+# Author(s): Elise. (add your name if you work on this)
+# Date started: 18 Jan 2016
+# Informatics Winter quarter 2016
+####
+
+# Not sure how to load files here... hmmm is this directory correct...
+EUSO2 <- read.csv("~EUSO2.csv")
