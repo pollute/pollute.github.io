eec2a9a36531446a8a12d922dc4f7d2ba5c824c2
Steph Griffin <sgriffin@bren.ucsb.edu>
2016-02-03 08:39:15 -0800

    edited format

--- AirPollution.R
+++ AirPollution.R
@@ -1,17 +1,23 @@
+#SO2 air pollution by EU city.
+#
 #This function summarizes the number of of cities that have air pollution levels 
 # in micrograms/cubic meter above a certain level.
 
+#' @param COUNTRY Country of interest, use code (2 letters) chosen from EUSO2 dataset
+#' @param LEVEL Concentration of SO2 pollution (ug/m^3) as threshold level
+#' @return OUTPUT Number of cities within the country exceeding threshold level
+#' @examples 
+#' COUNTRY = DE
+#' LEVEL = 20
+#' AirPollution(COUNTRY, LEVEL)
+#' @author Mitchell, Steph, Elise, Dannique
+
 AirPollution=function(COUNTRY,LEVEL){
 
-  #User defines the COUNTRY of interest, "Germany for example would need the input 'DE'
-  
-  #User also defines the LEVEL of pollution (SO2) that must be exceeded or equal to in analysis
-  
 library(readr)
 library(dplyr)
   
 EUSO2=read_csv('data/EUSO2.csv')
-  
   
 OUTPUT=EUSO2 %T>% 
   select(country_iso_code, city_name, ug_m3) %>% 
@@ -21,4 +27,6 @@ OUTPUT=EUSO2 %T>%
   summarize(n = n());
 return(OUTPUT)
 }
+
+
 
