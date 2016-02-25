a3d9eeefc01c23b91c3e7cd98c1390292e950ab9
sparrow925 <elise.k.wall@gmail.com>
2016-02-25 11:03:11 -0800

    LvlCities is fully functional now.

--- pollute/R/LvlCities.R
+++ pollute/R/LvlCities.R
@@ -17,13 +17,13 @@ LvlCities=function(COUNTRY='AT'){
   library(stringr)
   library(readxl)
   
-  levels=vector(length=5)
+  OUTPUT=vector(length=5)
   
   for(n in c(1, 2, 3, 4, 5)) {
     
     EUSO2=read_excel('data/EUSO22013.xlsx', sheet=n)
     
-    levels[n]=EUSO2 %>%
+    OUTPUT[n]=EUSO2 %>%
       filter(country_iso_code == COUNTRY) %>%
       select(µg_m3) %>%
       summarize(mean(µg_m3));
@@ -35,16 +35,3 @@ LvlCities=function(COUNTRY='AT'){
 }
 
 # library(testthat)
-# 
-# #Automated Test 1: If all ug_m3 observations < LEVEL, then output should be 0.
-# expect_that(str_length(NumCities('AT',50,'SO2')$n), equals(integer(0)))
-# 
-# #Automated Test 2: If all ug_m3 observations > LEVEL, then output should be all cities.
-# expect_that(length(NumCities('AT',0,'SO2')$n), equals(6))
-# 
-# #Automated Test 3: Should have 6 values for AT no matter pollution type.
-# expect_that(length(NumCities('AT',0,'SO2')$n), equals(6))
-# expect_that(length(NumCities('AT',0,'PM10')$n), equals(6))
-# expect_that(length(NumCities('AT',0,'PM2.5')$n), equals(6))
-# expect_that(length(NumCities('AT',0,'NO2')$n), equals(6))
-# expect_that(length(NumCities('AT',0,'O3')$n), equals(6))
