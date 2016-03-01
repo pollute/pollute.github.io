b1f842062b80ab3977e4f1d40fc1d23c13007109
sparrow925 <elise.k.wall@gmail.com>
2016-02-29 21:49:44 -0800

    First pass. Just need to ge the map to show up (easy right) WIll revisit tomorrow.

--- EUPollute/app.R
+++ EUPollute/app.R
@@ -2,43 +2,100 @@
 # Authors: Elise, Steph, Mitchell
 # Description: This is a Shiney app that takes pollutant data for five pollutants across the EU and displays concentration level by country. You can select which pollutant to display by dropdown.
 #
-#
+
+###############
+# Data Set up
+###############
 
 library(shiny)
+library(googleVis)
+library(RCurl)
+library(readxl)
+library(dplyr)
+
+pollutants = c("PM10", "NO2", "O3", "PM2.5", "BaP", "SO2")
+
+PM10 = read_excel('data/EU2013.xlsx', sheet=1)%>%
+  mutate(iso2c=`country iso code`)%>%
+  group_by(iso2c)%>%
+  summarise(`Value`=round(mean(statistic_value), digits = 2))%>%
+  mutate(pol="PM10")
+
+NO2 = read_excel('data/EU2013.xlsx', sheet=2)%>%
+  mutate(iso2c=`country iso code`)%>%
+  group_by(iso2c)%>%
+  summarise(`Value`=round(mean(statistic_value), digits = 2))%>%
+  mutate(pol="O3")
+
+O3 = read_excel('data/EU2013.xlsx', sheet=3)%>%
+  mutate(iso2c=`country iso code`)%>%
+  group_by(iso2c)%>%
+  summarise(`Value`=round(mean(statistic_value), digits = 2))%>%
+  mutate(pol="O3")
+
+PM2.5 = read_excel('data/EU2013.xlsx', sheet=4) %>%
+  mutate(iso2c=`country iso code`)%>%
+  group_by(iso2c)%>%
+  summarise(`Value`=round(mean(statistic_value), digits = 2))%>%
+  mutate(pol="PM2.5")
+
+BaP = read_excel('data/EU2013.xlsx', sheet=5)%>%
+  mutate(iso2c=`country iso code`)%>%
+  group_by(iso2c)%>%
+  summarise(`Value`=round(mean(statistic_value), digits = 2))%>%
+  mutate(pol="BaP")
+
+SO2 = read_excel('data/EU2013.xlsx', sheet=6)%>%
+  mutate(iso2c=`country iso code`)%>%
+  group_by(iso2c)%>%
+  summarise(`Value`=round(mean(statistic_value), digits = 2))%>%
+  mutate(pol="SO2")
+
+poltot = bind_rows(list(PM10, NO2, O3, PM2.5, BaP, SO2))
+
+####################
+# Shiney App
+####################
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Common EU Pollutants (2013)"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         selectInput("poll",
+                     "Pollutant to display:",
+                     pollutants)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         # Tell it to generate the map???
       )
    )
 ))
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- shinyServer(function(input, output) {
+   choose <- reactive({
+     input$poll})
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   output$gvis <- renderGvis({
+      pmap <- poltot %>%
+        subset(pol=choose)
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      gvisGeoChart(
+        pmap,locationvar = "iso2c", colorvar = "Average Value per m3",
+        options=list(
+          title ="Concentrations",
+          region='150',
+          width=1200, height=800,
+          backgroundColor.fill = "#BDBDBD",
+          colorAxis="{minvalue: 50, colors:['#FFEBEE', '#F44336']}"))
+      
    })
 })
 
